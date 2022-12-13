3129623f3a0e831c3b9fc9886d02f7fce7bab55e
Alex <alextufielamigo@gmail.com>
2022-12-13 13:49:28 -0400

    inicio de diseño registro de imagenes

--- src/controllers/fotosController.js
+++ src/controllers/fotosController.js
@@ -3,5 +3,8 @@ const controllers={}
 controllers.indexfotos=async(req,res)=>{
     res.render('pages/fotos/lista_fotos')
 }
+controllers.registroFotos=async(req,res)=>{
+    res.render('pages/fotos/registrar_foto')
+}
 
 module.exports=controllers
--- src/models/fotosRouter.js
+++ src/models/fotosRouter.js
@@ -3,5 +3,7 @@ const router=express.Router()
 const fotosControllers=require('../controllers/fotosController')
 
 router.get('/fotos',fotosControllers.indexfotos)
+router.get('/registro-fotos',fotosControllers.registroFotos)
+
 
 module.exports=router
