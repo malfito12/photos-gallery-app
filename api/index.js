4e22598ccb04645992cfc30abbd9e3b0506f99ef
Alex Maraza <alextufielamigo@gmail.com>
2023-01-09 19:26:11 -0400

    cambios

--- api/index.js
+++ api/index.js
@@ -6,9 +6,9 @@ const path=require('path')
 
 
 //SETTING
-app.use(express.json({limit:'50mb'}));
+app.use(express.json({limit:'40mb'}));
 app.use(express.urlencoded({
-    limit:'50mb',
+    limit:'40mb',
     extended:true
 }))
 app.set('port', process.env.PORT || 3000)
